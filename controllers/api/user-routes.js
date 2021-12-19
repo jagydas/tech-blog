747bf5a4b3b72584d55a146644dc9c4d90521ff3
Jagy <jagy.das12@gmail.com>
2021-12-19 13:35:30 -0500

    code check-in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 router.get('/', (req, res) => {
     User.findAll({
-            attributes: { exclude: ['[password'] }
+            attributes: { exclude: ['password'] }
         })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
